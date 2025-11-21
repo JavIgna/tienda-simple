4128412e4b5fe4c5a2ce50a06b2de666b3befc39
Javier Navarrete <44924176+JavIgna@users.noreply.github.com>
2025-11-20 21:37:36 -0300

    Subiendo trabajo en clases

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    let contador = $("#contadorCarrito").text();
+
+    console.log(contador)
+    $('.botonAniadir').on('click',
+        function () {
+            contador++;
+
+            $("#contadorCarrito").text(contador)
+        }
+    )
+})
